b16b705eb8ea66a9feb1ae8c7080be5c7ccd5d6c
Juho Pennanen <juho.pennanen@ecomond.com>
2023-12-10 11:06:26 +0200

    update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -1,6 +1,5 @@
 @echo off
 cls
 javac -d bin -cp "lib/json.jar;lib/engine.jar" -sourcepath src src/main/*.java
-javac -d bin -cp "lib/json.jar;lib/engine.jar" -sourcepath src src/test/*.java
 jar cfm MyGame.jar manifest.mf -C bin . -C lib .
 java -jar MyGame.jar
