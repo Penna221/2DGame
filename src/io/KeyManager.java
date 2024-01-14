77459a5ba6c80ee808fd77c14151cea2d3a0851d
Juho Pennanen <juho.pennanen@ecomond.com>
2024-01-14 15:43:23 +0200

    change input to wasd

--- src/io/KeyManager.java
+++ src/io/KeyManager.java
@@ -18,16 +18,16 @@ public class KeyManager implements KeyListener{
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
                 left = true;
                 break;
-            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
                 right = true;
                 break;
-            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
                 up = true;
                 break;
-            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
                 down = true;
                 break;
             case KeyEvent.VK_F2:
@@ -48,16 +48,16 @@ public class KeyManager implements KeyListener{
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
                 left = false;
                 break;
-            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
                 right = false;
                 break;
-            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
                 up = false;
                 break;
-            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
                 down = false;
                 break;
             default:
