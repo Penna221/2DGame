67e18463f44df981e12b422b6fcf402d76a96624
Juho Pennanen <juho.pennanen@ecomond.com>
2024-07-15 11:21:33 +0300

    Enemy aiming at player

--- src/entities/ai/Enemy1AI.java
+++ src/entities/ai/Enemy1AI.java
@@ -11,6 +11,7 @@ import world.World;
 
 public class Enemy1AI extends AI {
     private int rot = 0;
+    private boolean aiming = false;
     private Ellipse2D.Double tooCloseRect;
     private BufferedImage originalTexture;
     public Enemy1AI(Entity entity) {
@@ -28,23 +29,42 @@ public class Enemy1AI extends AI {
     @Override
     public void update() {
         e.texture = e.currentAnimation.getFrame();
-        rot++;
         if(e.texture !=null){
             e.texture = Factory.rotateImage(e.texture, rot);
         }
-        tooCloseRect = e.generateSurroundingCircle(200);
+        tooCloseRect = e.generateSurroundingCircle(500);
         if(tooCloseRect.intersects(World.player.bounds)){
             e.setAnimation(e.info.animations.get("hostile"));
+            aiming = true;
+            aimAtPlayer();
         }else{
             e.setAnimation(e.info.animations.get("idle"));
+            aiming = false;
         }
         e.currentAnimation.animate();
     }
+    private void aimAtPlayer(){
+        double px = World.player.x;
+        double py = World.player.y;
+        //CenterX and centerY
+        double cx = e.x+e.bounds.width/2;
+        double cy = e.y+e.bounds.height/2;
+        double angle = Math.atan2(py - cy, px - cx);
+        rot = (int)Math.toDegrees(angle);
+        System.out.println(rot);
 
+    }
     @Override
     public void render(Graphics g) {
         g.drawOval((int)(tooCloseRect.x - World.camera.getXOffset()),(int) (tooCloseRect.y - World.camera.getYOffset()), (int)tooCloseRect.width,(int)tooCloseRect.height);
-        
+        if(aiming){
+            double px = World.player.x;
+            double py = World.player.y;
+            //CenterX and centerY
+            double cx = e.x+e.bounds.width/2;
+            double cy = e.y+e.bounds.height/2;
+            g.drawLine((int)(cx- World.camera.getXOffset()), (int)(cy- World.camera.getYOffset()),(int)(px- World.camera.getXOffset()), (int)(py- World.camera.getYOffset()));
+        }
     }
     
 }
