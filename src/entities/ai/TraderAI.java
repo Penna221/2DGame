24af946bf26f5ed75fcaffec32bbf335c1c8d66f
Juho Pennanen <juho.pennanen@ecomond.com>
2024-07-15 18:01:13 +0300

    Starting interaction with trader

--- src/entities/ai/TraderAI.java
+++ src/entities/ai/TraderAI.java
@@ -1,17 +1,23 @@
 package entities.ai;
 
 import java.awt.Graphics;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 
 import entities.Entity;
+import gfx.AssetStorage;
+import world.World;
 
 public class TraderAI extends AI{
-
+    private Ellipse2D.Double interactCircle;
+    private boolean talk = false;
     public TraderAI(Entity entity) {
         super(entity);
     }
 
     @Override
     public void lateInit() {
+        interactCircle = e.generateSurroundingCircle(150);
         e.currentAnimation = e.info.animations.get("idle");
         e.currentAnimation.restart();
     }
@@ -19,12 +25,27 @@ public class TraderAI extends AI{
     @Override
     public void update() {
         e.texture = e.currentAnimation.getFrame();
+
+        if(interactCircle.intersects(World.player.bounds)){
+            talk = true;
+        }else{
+            talk = false;
+        }
+
+
         e.currentAnimation.animate();
     }
 
     @Override
     public void render(Graphics g) {
-        
+        g.drawOval((int)(interactCircle.x - World.camera.getXOffset()),(int) (interactCircle.y - World.camera.getYOffset()), (int)interactCircle.width,(int)interactCircle.height);
+        if(talk){
+            drawTalkBox(g);
+        }
     }
-    
+
+    private void drawTalkBox(Graphics g){
+        BufferedImage img = AssetStorage.images.get("interact_e");
+        g.drawImage(img,(int) (e.x +e.bounds.width/2 - World.camera.getXOffset()-img.getWidth()/2 ), (int)(e.y-World.camera.getYOffset() - img.getHeight()), null);
+    }
 }
